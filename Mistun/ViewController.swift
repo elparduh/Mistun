f39d1276faafd0ddd5fa31d18eef10d93b63e0eb
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-10-25 22:38:41 -0600

    first commit

--- Mistun/ViewController.swift
+++ Mistun/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Mistun
-//
-//  Created by Juan Ticante Vicente on 25/10/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
 
